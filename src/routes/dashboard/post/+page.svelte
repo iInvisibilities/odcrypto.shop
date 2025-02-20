83bfcadf5a5b5b1d8456a9c77a452d8c7112607e
iInvisibilities <louu3a@gmail.com>
2025-02-20 15:24:20 +0100

    create form with the multipart/form-data enctype to be used with Multer?

---
+++ src/routes/dashboard/post/+page.svelte
@@ -0,0 +1,8 @@
+<form action="" method="POST" enctype="multipart/form-data">
+	<input type="file" />
+	<input type="text" />
+	<textarea name="" id=""></textarea>
+	<input type="number" />
+
+	<input type="submit" />
+</form>
